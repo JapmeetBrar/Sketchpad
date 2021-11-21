2eef978c7a62f9127edc941d9617413d3c13c4bc
Japmeet Brar <Japmeetb@hotmail.com>
2021-11-20 23:57:35 -0500

    V1. Basic functions all working) cancel

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const container = document.querySelector('.container');
+const clearBtn = document.querySelector('.clear');
 
-const divs = [];
+let numSquares = 64;
+let divs;
 
-let numSquares = 16;
+clearBtn.addEventListener('click', resetGame);
 
-for (let i = 0; i < numSquares; i++) {
-    let temp = document.createElement('div');
-    temp.className="sketch";
-    temp.textContent=i;
-    temp.style.width=960/numSquares;
-    temp.style.height=960/numSquares;
-    container.appendChild(temp);
+createBoard(numSquares);
+
+function createBoard(squares){
+    for (let i = 0; i < numSquares*numSquares; i++) {
+        let temp = document.createElement('div');
+        temp.className="sketch";
+        temp.style.height= 960/numSquares + "px";
+        temp.style.width= 960/numSquares + "px";
+        temp.classList.add('hover');
+        container.appendChild(temp);
+
+        temp.addEventListener("mouseenter", function(e){
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            // temp.style.backgroundColor = "#"+ randomColor;
+            temp.style.backgroundColor = "black";
+        })
+    }  
+    divs = document.querySelectorAll('.hover');
+}
+
+
+function resetGame(){
+    divs.forEach((div)=>{
+        container.removeChild(div);
+    })
+    numSquares = prompt('Enter the number of squares you would like per side');
+    // while (numSquares>100){
+    //     numSquares =prompt('Please Enter a maxiumum of 100')
+    // }
+    createBoard(numSquares);
 }
